04980d3a2f781e530c5c4bdb86ca6a1f4b293ca5
Jiri Kuncar <jiri.kuncar@gmail.com>
2021-02-09 12:05:05 +0100

    fix traces

--- features/support/tracer.ts
+++ features/support/tracer.ts
@@ -1,44 +1,79 @@
 import { tracer } from 'dd-trace';
+import path from 'path';
 
 tracer.init();
 
-let PickleRunner = require('@cucumber/cucumber/lib/runtime/pickle_runner').default
-let scenarioFunction = PickleRunner.prototype.run
-let stepFunction = PickleRunner.prototype.runStep
+const SAMPLING_RULE_DECISION = require('dd-trace/packages/dd-trace/src/constants');
+const {
+    TEST_TYPE,
+    TEST_NAME,
+    TEST_SUITE,
+    TEST_STATUS,
+    getTestEnvironmentMetadata
+} = require('dd-trace/packages/dd-trace/src/plugins/util/test');
 
-PickleRunner.prototype.run = tracer.wrap(
-    'cucumber.test',
-    { type: 'test' },
-    async function (this: void, ...args: any[]) {
-        let pickleRunner = (this as any)
-        console.log(this);
-        
-        let span = (tracer.scope().active() as any)
-        span.resource = pickleRunner.pickle.name
-        span.setTag('test.name', pickleRunner.pickle.name)
-    
-        let result = await scenarioFunction.apply(this, args);
-        
-        console.log(result)
-        return result
+let PickleRunner = require('@cucumber/cucumber/lib/runtime/pickle_runner').default;
+let scenarioFunction = PickleRunner.prototype.run;
+let stepFunction = PickleRunner.prototype.runStep;
+
+
+function setStatusFromResult(span: any, result: any) {
+    if (result.status == 1) {
+        span.setTag(TEST_STATUS, 'pass');
+    } else if (result.status == 2) {
+        span.setTag(TEST_STATUS, 'skip');
+    } else {
+        span.setTag(TEST_STATUS, 'fail');
+        span.setTag('error.msg', result.message);
     }
-);
+}
 
+async function handleRun(this: void, ...args: any[]) {
+    let pickleRunner = (this as any);
+    let testName = pickleRunner.pickle.name;
+    let testSuite = path.relative(process.cwd(), pickleRunner.pickle.uri);
+    const testEnvironmentMetadata = getTestEnvironmentMetadata('cucumber');
 
-PickleRunner.prototype.runStep = tracer.wrap(
-    'cucumber.step',
-    { type: 'test' },
-    async function (this: void, ...args: any[]) {
-        console.log(args);
-        let result = await stepFunction.apply(this, args);
-        if (!args[0].isHook) {
-            let span = (tracer.scope().active() as any)
-            span.resource = args[0].pickleStep?.text
-            span.setTag('test.name', args[0].pickleStep.text)
+    const commonSpanTags = {
+        [TEST_TYPE]: 'test',
+        [TEST_NAME]: testName,
+        [TEST_SUITE]: testSuite,
+        [SAMPLING_RULE_DECISION]: 1,
+        ...testEnvironmentMetadata
+    }
+
+    return await tracer.trace(
+        'cucumber.test',
+        {
+            type: 'test',
+            resource: testName,
+            tags: commonSpanTags
+        },
+        async (span: any) => {
+            let result = await scenarioFunction.apply(pickleRunner, args);
+            setStatusFromResult(span, pickleRunner.getWorstStepResult());
+            return result;
         }
-        console.log(result)
-        return result
-    }
-);
+    );
+}
+
+async function handleRunStep(this: void, ...args: any[]) {
+    let step = (this as any)
+    console.log(args)
+    let resource = args[0].isHook ? 'hook' : args[0].pickleStep?.text
+    return await tracer.trace(
+        'cucumber.step',
+        { type: 'test', resource: resource },
+        async (span: any) => {
+            let result = await stepFunction.apply(step, args);
+            setStatusFromResult(span, result);
+            return result;
+        }
+    );
+}
+
+PickleRunner.prototype.run = handleRun;
+PickleRunner.prototype.runStep = handleRunStep;
+// TODO store the original methods
 
 export default tracer;
